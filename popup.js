455ead732bc4259d1f22af5e177c4d8ac3b6bc8e
ErradiAbderrahmnan99 <abderahmaneradi99@gmail.com>
2024-05-17 12:54:42 +0100

    eidt popup

--- popup.js
+++ popup.js
@@ -1,19 +1,21 @@
 document.getElementById('colorPicker').addEventListener('input', (event) => {
-    const color = event.target.value;
-    changeColor(color);
-  });
-  
-  function changeColor(color) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        func: setBackgroundColor,
-        args: [color]
-      });
+  const color = event.target.value;
+  changeColor(color);
+});
+
+function changeColor(color) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: setBackgroundColor,
+      args: [color]
     });
-  }
-  
-  function setBackgroundColor(color) {
-    document.body.style.backgroundColor = color;
-  }
-  
+  });
+}
+
+function setBackgroundColor(color) {
+  var elems = document.body.querySelectorAll("*");
+  elems.forEach(element => {
+    element.style.backgroundColor = color;
+  });
+}
